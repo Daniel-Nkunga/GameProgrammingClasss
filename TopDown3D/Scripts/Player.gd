5924722aaff3730e8d35df12ad938d1fff62ca1e
Daniel-Nkunga <144631064+Daniel-Nkunga@users.noreply.github.com>
2024-03-11 19:55:46 -0500

    Done with initial viewport testing
    Not scaled/moved properly imo

--- TopDown3D/Scripts/Player.gd
+++ TopDown3D/Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends RigidBody3D
 
 @export var backdash_speed = 1500
-var cameraToggle = 0
+var cameraToggle = 1
 
 func _ready() -> void:
 	pass
@@ -11,22 +11,19 @@ func _process(delta: float) -> void:
 	input.x = Input.get_axis("ui_left", "ui_right")
 	input.z = Input.get_axis("ui_up", "ui_down")
 	apply_central_force((input * 1200.0 * delta))
-	if Input.is_action_just_pressed("mouse_left"):
-		var global_mouse_pos = get_viewport().get_mouse_position()
-		var local_mouse_pos = global_mouse_pos - global_transform.origin
-		apply_central_force(-local_mouse_pos.normalized() * 1500 * delta)
-		print("bang!")
 	
 	if Input.is_action_just_pressed("ui_enter"):
 		if cameraToggle == 0:
 			$Camera3D.position = Vector3(0, 0, 0)
-			print("Woosh")
 			cameraToggle += 1
 		elif cameraToggle == 1:
-			$Camera3D.position = Vector3(-1, 1, 0)
-			rotate($Camera3D(.5))
-			print("Woosh")
+			$Camera3D.position = Vector3(0, 1.5, 3)
+			$Camera3D.rotate(Vector3(0, 1.5, 3), deg_to_rad(-45))
 			cameraToggle += 1
+		elif cameraToggle == 2:
+			$Camera3D.position = Vector3(0, .5, 1)
+			$Camera3D.rotate(Vector3(0, 0.5, 1), deg_to_rad(0))
+			cameraToggle = 0
 
 
 		
